10f03ee6bd5b6e2dc887c7a1f34adb44dcc2b59a
Jeff Andony <jeff.andony@gmail.com>
2020-09-15 19:43:44 -0600

    removed unused files. put something on screen. updated readme.md file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Cards from "./components/Cards/Cards";
+
+import { Animated } from "react-animated-css";
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Animated animationIn="bounceInLeft" animationInDuration={1000} isVisible={true}>
+        <section id="hero">
+          <div className="overlay" />
+        </section>
+      </Animated>
+
+      <Animated animationIn="bounceInLeft" animationInDuration={1000} animationInDelay={1000} isVisible={true}>
+        <section id="boxes">
+          <Grid container alignItems="center">
+            {/* one */}
+            <Grid item xs={12} md={6} lg={3}>
+              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={3000} isVisible={true}>
+                <Cards />
+              </Animated>
+            </Grid>
+
+            {/* two */}
+            <Grid item xs={12} md={6} lg={3}>
+              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={3500} isVisible={true}>
+                <Cards />
+              </Animated>
+            </Grid>
+
+            {/* three */}
+            <Grid item xs={12} md={6} lg={3}>
+              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={4000} isVisible={true}>
+                <Cards />
+              </Animated>
+            </Grid>
+
+            {/* four */}
+            <Grid item xs={12} md={6} lg={3}>
+              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={4500} isVisible={true}>
+                <Cards />
+              </Animated>
+            </Grid>
+          </Grid>
+        </section>
+      </Animated>
+
+      <Animated animationIn="bounceInLeft" animationInDuration={1000} animationInDelay={2000} isVisible={true}>
+        <section id="about">
+          Section 3
+      </section>
+      </Animated>
+    </div >
   );
 }
 
