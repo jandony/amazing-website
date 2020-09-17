58cbc8ed0aa3ce7414c59b62c7011cb75f4f5ba9
Jeff Andony <jeff.andony@gmail.com>
2020-09-16 21:24:29 -0600

    uploading infinite loop issue

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ _by Jeffrey Andony_
 ## Frameworks
 
 1. Material UI
+
+## Site Features
+
+1. Loading page animation
+2. Page or component transitions
+3. Horizontal scrolling feature
+4. Swiping feature
+5. Navigation tabs
+6. Full screen multi-step form or navigation
+7. Video backgrounds
+8. Social media sign ins
+9. Parallax scrolling
--- src/App.js
+++ src/App.js
@@ -1,47 +1,114 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
+import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
 import Cards from "./components/Cards/Cards";
 
 import { Animated } from "react-animated-css";
 
 function App() {
+  const [visibleState, setVisibleState] = useState({
+    hero: true,
+    section2: true,
+    section3: false,
+    section4: false,
+    section5: false,
+    section6: false,
+    section7: false,
+    section8: false,
+  });
+
+  useEffect(() => {
+    // selects all section html tags
+    const mySections = document.querySelectorAll("section");
+
+    // midpoint of the viewport height. Also triggers at 50% of viewport height
+    const options = {
+      threshold: 0,
+      rootMargin: "-50%",
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        // if the entry is at the midpoint, then...
+        if (entry.isIntersecting) {
+          console.log("intersected!");
+          setVisibleState({ [entry.target.id]: true });
+          // else, do nothing
+        } else {
+          return;
+        }
+      });
+    }, options);
+
+    // our "listener" for each section
+    mySections.forEach((section) => {
+      observer.observe(section);
+    });
+  }, [visibleState]);
 
   return (
     <div className="App">
-
-      <Animated animationIn="bounceInLeft" animationInDuration={1000} isVisible={true}>
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        isVisible={visibleState.section1}
+      >
         <section id="hero">
           <div className="overlay" />
         </section>
       </Animated>
 
-      <Animated animationIn="bounceInLeft" animationInDuration={1000} animationInDelay={1000} isVisible={true}>
-        <section id="boxes">
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={1000}
+        isVisible={visibleState.section2}
+      >
+        <section id="section2">
           <Grid container alignItems="center">
             {/* one */}
-            <Grid item xs={12} md={6} lg={3}>
-              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={3000} isVisible={true}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Animated
+                animationIn="fadeInUp"
+                animationInDuration={1000}
+                animationInDelay={2000}
+                isVisible={true}
+              >
                 <Cards />
               </Animated>
             </Grid>
 
             {/* two */}
-            <Grid item xs={12} md={6} lg={3}>
-              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={3500} isVisible={true}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Animated
+                animationIn="fadeInUp"
+                animationInDuration={1000}
+                animationInDelay={2000}
+                isVisible={true}
+              >
                 <Cards />
               </Animated>
             </Grid>
 
             {/* three */}
-            <Grid item xs={12} md={6} lg={3}>
-              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={4000} isVisible={true}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Animated
+                animationIn="fadeInUp"
+                animationInDuration={1000}
+                animationInDelay={2000}
+                isVisible={true}
+              >
                 <Cards />
               </Animated>
             </Grid>
 
             {/* four */}
-            <Grid item xs={12} md={6} lg={3}>
-              <Animated animationIn="fadeInUp" animationInDuration={1000} animationInDelay={4500} isVisible={true}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <Animated
+                animationIn="fadeInUp"
+                animationInDuration={1000}
+                animationInDelay={2000}
+                isVisible={true}
+              >
                 <Cards />
               </Animated>
             </Grid>
@@ -49,12 +116,70 @@ function App() {
         </section>
       </Animated>
 
-      <Animated animationIn="bounceInLeft" animationInDuration={1000} animationInDelay={2000} isVisible={true}>
-        <section id="about">
-          Section 3
-      </section>
+      <Animated
+        className="section3Animated"
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section3}
+      >
+        <section id="section3">Section 3</section>
       </Animated>
-    </div >
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section4}
+      >
+        <section id="section4">Section 4</section>
+      </Animated>
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section5}
+      >
+        <section id="section5">Section 5</section>
+      </Animated>
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section6}
+      >
+        <section id="section6">Section 6</section>
+      </Animated>
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section7}
+      >
+        <section id="section7">Section 7</section>
+      </Animated>
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={visibleState.section8}
+      >
+        <section id="section8">Section 8</section>
+      </Animated>
+
+      <Animated
+        animationIn="bounceInLeft"
+        animationInDuration={1000}
+        animationInDelay={0}
+        isVisible={true}
+      >
+        <section></section>
+      </Animated>
+    </div>
   );
 }
 
